3a7a67b05c91098080c682eb58dce3f464f99812
Ian Whitney <iwhitney@ssa-i.org>
2013-06-18 14:18:06 -0500

    Using active_support/concern for a more modern way of including this gem

--- lib/hstore_radio_buttons.rb
+++ lib/hstore_radio_buttons.rb
@@ -1,13 +1,12 @@
 require "hstore_radio_buttons/version"
+require 'active_support/concern'
 
 module HstoreRadioButtons
+  extend ActiveSupport::Concern
+
   module ClassMethods
     def hstore_radio_buttons
-
+      self.has_one :hstore_radio_data
     end
-  end
-
-  def self.included(receiver)
-    receiver.extend      ClassMethods
   end
 end
