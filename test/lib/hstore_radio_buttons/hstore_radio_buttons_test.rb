b89c19fc5792fcaae5e5481f01db6c105fcf1cdc
Ian Whitney <iwhitney@ssa-i.org>
2013-06-18 15:03:11 -0500

    Using better minitest expectation

--- test/lib/hstore_radio_buttons/hstore_radio_buttons_test.rb
+++ test/lib/hstore_radio_buttons/hstore_radio_buttons_test.rb
@@ -2,10 +2,10 @@ require_relative '../../test_helper.rb'
 
 describe HstoreRadioButtons do
   it 'allows a class to respond to hstore_radio_buttons' do
-    Person.respond_to?(:hstore_radio_buttons).must_equal true
+    Person.must_respond_to :hstore_radio_buttons
   end
 
   it 'builds a has_one association with the hstore_radio_data table' do
-    Person.new.respond_to?(:hstore_radio_data).must_equal true
+    Person.new.must_respond_to :hstore_radio_data
   end
 end
