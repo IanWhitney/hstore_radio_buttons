62dab4f28abaf6f7d52027c5f135f2dfc9bc9891
Ian Whitney <iwhitney@ssa-i.org>
2013-06-18 11:54:52 -0500

    Adding version test

--- test/lib/hstore_radio_buttons/version_test.rb
+++ test/lib/hstore_radio_buttons/version_test.rb
@@ -1,7 +1,7 @@
 require_relative '../../test_helper.rb'
 
-#describe HstoreRadioButtons do
-  #it "must have a version" do
-    #HstoreRadioButtons::Version.wont_be_nil
-  #end
-#end
+describe HstoreRadioButtons do
+  it "must have a version" do
+    HstoreRadioButtons::VERSION.wont_be_nil
+  end
+end
