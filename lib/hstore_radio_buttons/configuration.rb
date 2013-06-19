4b4d94421705fee232c7fa84a2ad90ede915a144
Ian Whitney <iwhitney@ssa-i.org>
2013-06-19 14:37:09 -0500

    Not sure what the purpose of that struct was, but since I'm not using it I'm removing it.

--- lib/hstore_radio_buttons/configuration.rb
+++ lib/hstore_radio_buttons/configuration.rb
@@ -1,11 +1,9 @@
 module HstoreRadioButtons
   class Configuration
-    Config = Struct.new(:button_sets)
-
     def self.from_yaml(model, yaml_file_location = './config/hstore_radio_button_sets.yml')
       if yaml_exists?(yaml_file_location)
         button_sets = YAML.load(config_file(yaml_file_location))[model.to_s.downcase].keys
-        Config.new(button_sets).button_sets.each do |button_set|
+        button_sets.each do |button_set|
           model.send(:define_method, button_set.to_sym) {
             hstore_data_proxy[button_set]
           }
