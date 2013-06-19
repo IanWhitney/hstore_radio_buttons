c3a65061402ba4969023e70681ec9c44b00ed936
Ian Whitney <iwhitney@ssa-i.org>
2013-06-19 13:23:27 -0500

    Just a simple check that our data table is in place

--- test/lib/hstore_radio_buttons/hstore_radio_buttons_test.rb
+++ test/lib/hstore_radio_buttons/hstore_radio_buttons_test.rb
@@ -2,7 +2,7 @@ require_relative '../../test_helper.rb'
 
 describe HstoreRadioButtons do
   it 'adds a HstoreRadioData active record class' do
-    HstoreRadioButtons::HstoreRadioData.new.must_equal []
+    HstoreRadioButtons::HstoreRadioData.new.wont_be_nil
   end
 
   it 'allows a class to respond to hstore_radio_buttons' do
@@ -12,6 +12,5 @@ describe HstoreRadioButtons do
   it 'builds a has_one association with the hstore_radio_data table' do
     Person.new.must_respond_to :hstore_radio_data
     Person.new.must_respond_to :build_hstore_radio_data
-    #Person.new.build_hstore_radio_data.must_equal {}
   end
 end
