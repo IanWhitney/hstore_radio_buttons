3d05a509dc4c93e0725dcfff22def83fe228f037
Ian Whitney <iwhitney@ssa-i.org>
2013-06-19 14:53:03 -0500

    Moving the build to an instance method where should have been to begin with.

--- lib/hstore_radio_buttons/configuration.rb
+++ lib/hstore_radio_buttons/configuration.rb
@@ -1,27 +1,37 @@
 module HstoreRadioButtons
   class Configuration
     def self.from_yaml(model, yaml_file_location = './config/hstore_radio_button_sets.yml')
-      if yaml_exists?(yaml_file_location)
-        button_sets = YAML.load(config_file(yaml_file_location))[model.to_s.downcase].keys
-        button_sets.each do |button_set|
-          model.send(:define_method, button_set.to_sym) {
-            hstore_data_proxy[button_set]
+      self.new._from_yaml(model, yaml_file_location)
+    end
+
+    def _from_yaml(model, yaml_file_location)
+      self.yaml_file_location = yaml_file_location
+      self.model = model
+
+      if yaml_exists?
+        button_definitions_from_yaml.each do |button_definition|
+          model.send(:define_method, button_definition.to_sym) {
+            hstore_data_proxy[button_definition]
           }
-          model.send(:define_method, "#{button_set}=".to_sym) {|value|
-            hstore_data_proxy[button_set] = value
+          model.send(:define_method, "#{button_definition}=".to_sym) {|value|
+            hstore_data_proxy[button_definition] = value
           }
         end
       end
     end
 
-    private
-
-    def self.config_file(yaml_file_location)
+  private
+    attr_accessor :model, :yaml_file_location
+    def config_file
       File.open(yaml_file_location)
     end
 
-    def self.yaml_exists?(yaml_file_location)
+    def yaml_exists?
       File.exists?(yaml_file_location)
+    end
+
+    def button_definitions_from_yaml
+      YAML.load(config_file)[model.to_s.downcase].keys
     end
   end
 end
