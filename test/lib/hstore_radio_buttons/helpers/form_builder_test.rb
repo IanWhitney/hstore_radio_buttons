09a46e2621649df01c1ed26473e5fb8e4531b436
Ian Whitney <iwhitney@ssa-i.org>
2013-06-25 08:19:57 -0500

    basic response test for FormBuilder method

--- test/lib/hstore_radio_buttons/helpers/form_builder_test.rb
+++ test/lib/hstore_radio_buttons/helpers/form_builder_test.rb
@@ -2,7 +2,15 @@ require_relative '../../../test_helper.rb'
 require 'action_view/test_case'
 
 class FormBuilderTest < ActionView::TestCase
-  test "should do something" do
-   assert true
+  test "adds my FormHelper to Rails' FormHelper" do
+    assert ActionView::Helpers::FormHelper.include?(HstoreRadioButtons::FormHelper)
+    assert ActionView::Helpers::FormBuilder.include?(HstoreRadioButtons::FormBuilder)
+    assert ActionView::Base.include?(HstoreRadioButtons::FormHelper)
+  end
+
+  test "FormHelper instances can respond to hstore_radio_button" do
+    template = Object.new
+    it = ActionView::Helpers::FormBuilder.new(:person, Person.new, template, {:url => 'test'}, proc {})
+    assert it.respond_to?(:hstore_radio_button)
   end
 end
