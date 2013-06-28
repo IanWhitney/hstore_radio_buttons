2a223655705526140f9a51215ece8dabe5fc2860
Ian Whitney <iwhitney@ssa-i.org>
2013-06-28 11:09:37 -0500

    Fix attempt for an integration project

--- lib/hstore_radio_buttons/button_options.rb
+++ lib/hstore_radio_buttons/button_options.rb
@@ -2,7 +2,7 @@ module HstoreRadioButtons
   class ButtonOptions
     attr_accessor :name, :options
     def initialize(name, options)
-      self.name = name
+      self.name = name.to_s
       self.options = *options
     end
   end
