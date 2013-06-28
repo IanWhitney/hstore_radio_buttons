82d27e6359a95bdcc417e745846afc192e427eeb
Ian Whitney <iwhitney@ssa-i.org>
2013-06-28 13:37:50 -0500

    Revert "Fix attempt for an integration project"
    This reverts commit 2a223655705526140f9a51215ece8dabe5fc2860.

--- lib/hstore_radio_buttons/button_options.rb
+++ lib/hstore_radio_buttons/button_options.rb
@@ -2,7 +2,7 @@ module HstoreRadioButtons
   class ButtonOptions
     attr_accessor :name, :options
     def initialize(name, options)
-      self.name = name.to_s
+      self.name = name
       self.options = *options
     end
   end
