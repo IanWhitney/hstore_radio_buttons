bb89b573e5ad7d78abcc8feb0459813b30c0b61e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-10 19:56:56 +0800

    Delete lib directory

--- lib/hstore_radio_buttons/hstore_radio_data.rb
+++
@@ -1,7 +0,0 @@
-require 'activerecord-postgres-hstore'
-module HstoreRadioButtons
-  class HstoreRadioData < ActiveRecord::Base
-    belongs_to :model, :polymorphic => true
-    serialize :hstore_data, ActiveRecord::Coders::Hstore
-  end
-end
--- lib/hstore_radio_buttons/version.rb
+++
@@ -1,3 +0,0 @@
-module HstoreRadioButtons
-  VERSION = "0.0.4"
-end
